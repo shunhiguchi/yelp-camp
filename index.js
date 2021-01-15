81e19dc0e3e68b40a150af12a03ce58c6d793dba
shunhiguchi <shun.higuchi@outlook.com>
2021-01-14 21:43:21 -0500

    Add seed utilities

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const ejs = require("ejs");
 
+const Campground = require("./models/campground.js");
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Established connection to database")
+});
+
+app.get("/", (req, res) => {
+    console.log("Hello from YelpCamp!");
+})
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({title: "My Backyard", description: "Cheap camping"});
+    await camp.save();
+    res.send(camp);
+})
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Campground = require("../models/campground.js");
+const cities = require("./cities.js");
+const {places, descriptors} = require("./seedHelpers.js");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Established connection to database")
+});
+
+const sample = (array) => array[Math.random() * array.length];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++)
+    {
+        const n = Math.floor(Math.random() * 1000);
+        const camp = new Campground( 
+        {
+            location: `${cities[n].city}, ${cities[n].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
