a7821ce24653f803d17cc3a69eabc24990c3d43a
shunhiguchi <shun.higuchi@outlook.com>
2021-02-05 21:48:05 -0500

    Update authentication
    Add register, login, and logout to navigation bar

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,16 @@ router.get("/register", (req, res) => {
     res.render("users/register.ejs");
 })
 
-router.post("/register", catchAsync(async (req, res) => {
+router.post("/register", catchAsync(async (req, res, next) => {
     try {
         const {email, username, password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user, password);
-        req.flash("success", "Welcome to YelpCamp!");
-        res.redirect("/campgrounds")
+        req.login(registeredUser, err => {
+            if (err) return next(error);
+            req.flash("success", "Welcome to YelpCamp!");
+            res.redirect("/campgrounds");
+        })
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/register");
@@ -28,6 +31,12 @@ router.get("/login", (req, res) => {
 
 router.post("/login", passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}), (req, res) => {
     req.flash("success", "Welcome back!");
+    res.redirect("/campgrounds");
+})
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.flash("success", "Goodbye!");
     res.redirect("/campgrounds");
 })
 
