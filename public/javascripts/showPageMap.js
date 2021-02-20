f572b8a7310e1a250119f44848af3613ccf935eb
shunhiguchi <shun.higuchi@outlook.com>
2021-02-19 19:34:47 -0500

    Update map
    Add popup to map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,11 +2,17 @@ mapboxgl.accessToken = mapboxToken;
 console.log(campground);
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: campground.geometry.coordinates,
     zoom: 9
 });
 
 const marker = new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
     .addTo(map);
