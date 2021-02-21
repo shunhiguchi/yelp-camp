868bdca458d416ac004d6b4895491f55da6a5ce0
shunhiguchi <shun.higuchi@outlook.com>
2021-02-21 13:52:24 -0500

    Add map control to cluster map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,11 +4,14 @@
 // https://account.mapbox.com
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function () {
     // Add a new source from our GeoJSON data and
